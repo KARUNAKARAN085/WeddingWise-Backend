824539d0222c1e0e22d9c7dac12c9ad76d3a971d
KARUNAKARAN085 <karunakaranrk2001@gmail.com>
2024-07-25 12:58:57 +0530

    in

---
+++ Routes/authRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { loginUser, registerUser,google } from "../Controllers/authController.js";
+
+const router = express.Router();
+
+router.get("/", (_, res) => {       
+    res.send("Welcome to Auth provider routes");
+});
+
+router.post("/register-user", registerUser);
+router.post("/login-user", loginUser );
+router.post("/google",google);
+
+export default router;
+
